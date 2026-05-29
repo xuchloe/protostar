2aa1b24ecfa09d06e32171013a3322dc2a72c1a5
Chloe Xu <chloex@mit.edu>
2026-05-28 20:16:56 -0700

    bug fix

--- src/auto_detect_test.py
+++ src/auto_detect_test.py
@@ -361,8 +361,8 @@ def best_sim_auto_detect(info, vis, n_sources: int = None, clean_output=True, co
 
     results = []
     if n_sources is not None:
-        for i in range(n_peaks): # assumption: summary more often has false positives than false negatives
-            temp = sim_auto_detect(vis=vis, info=info, n_sources=i+1, clean_output=clean_output, corner_plot=corner_plot)
+        for i in range(n_peaks,0,-1): # assumption: summary more often has false positives than false negatives
+            temp = sim_auto_detect(vis=vis, info=info, n_sources=i, clean_output=clean_output, corner_plot=corner_plot)
             if temp:
                 if results:
                     if results[0]['bic'] - temp[0]['bic'] > 10:
